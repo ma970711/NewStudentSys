cf36f5485155efd77e787a85633428e9776e497c
ma970711 <945382918@qq.com>
2019-06-26 22:33:15 +0800

    修复某些BUG

--- Application/controller/connDB.class.php
+++ Application/controller/connDB.class.php
@@ -3,7 +3,7 @@ function conn(){
 
         $dsn = "mysql:host=bloudecbrmwl.mysql.sae.sina.com.cn;port=10208;dbname=studentInfo";
         try {
-            $db = new PDO($dsn, '', '', array(PDO::ATTR_PERSISTENT => true));
+            $db = new PDO($dsn, 'admin', 'admin123', array(PDO::ATTR_PERSISTENT => true));
         }catch (PDOException $ex){
             die("不能连接数据库".$ex->getMessage());
         }
